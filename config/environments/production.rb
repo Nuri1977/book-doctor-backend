4048a3640f4d4bf95ad1aa746b73de3a21116de5
Nuri1977 <nurilacka@gmail.com>
2022-06-15 02:16:01 +0200

    Update production link for helpers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,9 @@
 require "active_support/core_ext/integer/time"
 
+Rails.application.routes.default_url_options = {
+  host: 'https://book-a-doctor.herokuapp.com'
+}
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
